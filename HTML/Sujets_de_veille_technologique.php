adbda0ccacc6fd9f55a3413320d9bdee239d031d
Spitskyyy <grall.emeric@ik.me>
2024-02-21 15:49:01 +0100

    Maj

--- HTML/Sujets_de_veille_technologique.php
+++ HTML/Sujets_de_veille_technologique.php
@@ -1,8 +1,62 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="../CSS/Style_Sujets_de_veille_technologique.css">
+<link rel="stylesheet" href="../Style/Style_Sujets_de_veille_technologique.css">
 <head>
     <title>Portfolio</title>
 </head>
+<header>
+<h1 align="center">Sujets-de-veille-technologique</h1>
+<hr>
+<table xborder="" width="100%">
+  <tr>
+    <td width="33%" align="center">
+      <div class="dropdown">
+      <p>Compétences</p>
+      <div class="dropdown-content">
+        <a href="/HTML/Slam.html">Développement</a>
+        <a href="/HTML/Sisr.html">Réseaux</a>
+        <a href="/HTML/Cyber.html">Cybersécurité</a>
+        </a>
+      </div>
+    </div>
+  </td>
+  <td width="33%" align="center">
+    <div class="dropdown">
+      <p>Certification</p>
+      <div class="dropdown-content">
+    </a>
+  </div>
+</div> 
+  </td>
+  
+  <td width="34%" align="center">
+    <a href="../index.html">Accueil</a>
+    </a>
+  </div>
+</div>  
+  </td>
+  </tr>
+</table>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <body>
 
     <?php
@@ -37,25 +91,6 @@
 
 
 
-
-
-    <table xborder="" width="100%">
-
-        <tr>
-            <td width="33%" align="center"><a href="../index.html">Présentation</a></td>
-            <td width="33%" align="center"><a href="Competences.html">Compétences</a></td>
-            <td width="33%" align="center"><a href="Certification.html">Certifications</a></td>
-        </tr>
-    </table>
-    <hr>
-<h1 align="center">Sujets-de-veille-technologique</h1>
-
 </body>
-<table xborder="" width="100%">
-    <tr>
-        <td align="center">Sujet-1</td>
-        <td align="center">Sujet-2</td>
-        <td align="center">Sujet-3</td>
-    </tr>
 </table>
 </html>
